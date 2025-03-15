a5b53620c56c131937a0ded6b50b8ef93c576ab9
DevJulio99 <devjulio26@gmail.com>
2025-03-15 17:10:08 -0500

    ajuste en texto evaluaciones

--- pages/evaluaciones/index.vue
+++ pages/evaluaciones/index.vue
@@ -50,9 +50,9 @@ onMounted(() => {
   <BaseLayout :rightAside="false" bgWhite>
     <BaseTitle text="evaluaciones" />
     <div class="bg-cyan_40 p-4 rounded font-nunito border border-cyan_80 border-l-[10px]">
-      <strong>¡La sección para rendir la evaluación de competencias generales ya está
+      <strong>¡La sección para rendir la evaluación de los cursos ya está
       disponible!</strong> Es importante tener en cuenta que una vez que comience, no
-      podrá acceder a otras secciones de la plataforma hasta terminar la
+      se podrá acceder a otras secciones de la plataforma hasta terminar la
       evaluación.
     </div>
 
