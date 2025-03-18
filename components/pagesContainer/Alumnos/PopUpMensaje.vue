4d0d1fc85869621900d9c9ccc9551dbeb85087ae
devjulio26 <devjulio26@gmail.com>
2025-03-17 19:43:13 -0500

    agregado de validaciones en vista alumnos y ajuste en tab de notas

--- components/pagesContainer/Alumnos/PopUpMensaje.vue
+++ components/pagesContainer/Alumnos/PopUpMensaje.vue
@@ -1,15 +1,21 @@
 <script lang="ts" setup>
 withDefaults(defineProps<{
     message: string;
+    type: 'success' | 'error'
 }>(),
 {
-  message: ""
+  message: "",
+  type: 'success'
 })
 </script>
 
 <template>
-    <div id="popupMsg" class="py-3 px-4 fixed z-[100] top-[20px] right-[-1000px] w-auto h-auto bg-error-dark text-white rounded-lg">
-    <h3>Error</h3>
+    <div id="popupMsg" class="py-3 px-4 fixed z-[100] top-[20px] right-[-1000px] w-auto h-auto text-white rounded-lg"
+      :class="{
+        'bg-error-dark': type == 'error',
+        'bg-green_70': type == 'success'
+      }">
+    <h3>{{type == 'success' ? 'Ok' : 'Error'}}</h3>
     <p>{{ message }}</p>
   </div>
 </template>
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,5 @@
+import { object } from "yup";
+
 export const decryptJWT = (token: string) => {
     const parts = token.split('.');
   
@@ -30,4 +32,22 @@ export const getUrls = (data: any[]) => {
   }
 
   return urls
+}
+
+export const validateForm = (form: object, optional: string[]) => {
+   const alumnoStore = useAlumnoStore();
+   const arraysForm = Object.entries(form);
+   const noOptional = arraysForm.filter(x => !optional.includes(x[0]));
+   const hiddenData = noOptional.filter(x => typeof(x[1]) == "string" ? x[1].trim().length <=0 : (typeof(x[1]) == "number" ? x[1] <= 0 : false))
+   let keysHiddenData = [];
+   //console.log('noHidden', noHidden);
+
+   for(let i = 0; i < hiddenData.length; i ++){
+    keysHiddenData.push(hiddenData[i][0]);
+   }
+   if(keysHiddenData.length){
+    alumnoStore.setErrorForm(keysHiddenData);
+    alumnoStore.setError(true, 'Tienes que completar los campos')
+   }
+   return keysHiddenData.length
 }
