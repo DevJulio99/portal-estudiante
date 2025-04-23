7d1de91090927d87ef391d817a202b1caeba4968
devjulio26 <devjulio26@gmail.com>
2025-04-23 14:37:16 -0500

    implemtacion de captcha

--- plugins/02.api.ts
+++ plugins/02.api.ts
@@ -20,6 +20,7 @@ import ImagenPagoModule from '~/repository/modules/ImagePago';
 import AlumnoModule from '~/repository/modules/Alumno';
 import DocumentoAddModule from '~/repository/modules/DocumentoAdd';
 import GradosModule from '~/repository/modules/Grados';
+import CaptchaModule from '~/repository/modules/Captcha';
 
 
 interface IApiInstance {
@@ -44,6 +45,7 @@ interface IApiInstance {
 	alumno: AlumnoModule;
 	agregarDocumento: DocumentoAddModule;
 	grados: GradosModule;
+	captcha: CaptchaModule;
 }
 
 export default defineNuxtPlugin(() => {
@@ -76,6 +78,7 @@ export default defineNuxtPlugin(() => {
 		alumno: new AlumnoModule(apiFecther),
 		agregarDocumento: new DocumentoAddModule(apiFecther),
 		grados: new GradosModule(apiFecther),
+		captcha: new CaptchaModule(apiFecther)
 	};
 
 	return {
