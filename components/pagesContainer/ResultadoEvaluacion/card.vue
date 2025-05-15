f87a3772a64ab198b9fa669f9f43b5aaf33cfccd
DevJulio99 <devjulio26@gmail.com>
2025-05-15 13:02:26 -0500

    revision en grafico de resultados evaluacion

--- components/pagesContainer/ResultadoEvaluacion/card.vue
+++ components/pagesContainer/ResultadoEvaluacion/card.vue
@@ -10,31 +10,50 @@ const props = withDefaults(
 {}
 );
 
-const valueStroke = !props.data ? 0 : 100 - (props.data.puntaje + valueEnBlanco);
-const dashOffset = props.data.porcentajeIncorrecto > props.data.porcentajeBlanco ? (props.data.porcentajeIncorrecto - props.data.porcentajeBlanco) : (props.data.porcentajeIncorrecto + props.data.porcentajeBlanco);
-</script>
+const porcentajeCorrectas = props.data.puntaje;
+const porcentajeIncorrectas = props.data.porcentajeIncorrecto;
+const porcentajeEnBlanco = props.data.porcentajeBlanco;
+
+const offsetIncorrectas = porcentajeCorrectas;
+const offsetEnBlanco = porcentajeCorrectas + porcentajeIncorrectas;</script>
 
 <template>
     <div v-if="data" class="relative rounded-3xl overflow-hidden bg-[#edf9f6] shadow-2xl">
         <div class="flex flex-col lg:flex-row">
             <div class="relative lg:w-1/3 p-10 bg-[#daf3ec] flex items-center justify-center">
                 <svg viewBox="0 0 36 36" class="w-[140px] h-[140px]">
-                    <path
-                        class="fill-none stroke-[#22C55E] stroke-[4.2]"
-                        d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <path
-                        class="circle-percentage stroke-[#F15656]"
-                        :stroke-dasharray="`${data.porcentajeIncorrecto}, 100`"
-                        d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <path
-                        class="circle-percentage stroke-[#FFE6FB]"
-                        :stroke-dasharray="`${data.porcentajeBlanco}, 100`"
-                        :stroke-dashoffset="`${data.porcentajeIncorrecto > 0 ? (dashOffset) : 0}`"
-                        d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
+                <path
+                    class="fill-none stroke-[#F1F5F9] stroke-[4.2]"
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                <path
+                    class="circle-percentage stroke-[#22C55E]"
+                    :stroke-dasharray="`${porcentajeCorrectas} ${100 - porcentajeCorrectas}`"
+                    stroke-dashoffset="25"
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                <path
+                    class="circle-percentage stroke-[#F15656]"
+                    :stroke-dasharray="`${porcentajeIncorrectas} ${100 - porcentajeIncorrectas}`"
+                    :stroke-dashoffset="25 - offsetIncorrectas"
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+                <path
+                    class="circle-percentage stroke-[#CBD5E1]"
+                    :stroke-dasharray="`${porcentajeEnBlanco} ${100 - porcentajeEnBlanco}`"
+                    :stroke-dashoffset="25 - offsetEnBlanco"
+                    d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
                 </svg>
+
                 <div class="absolute w-[105px] h-[105px] bg-white rounded-full flex items-center justify-center shadow-lg">
                     <div class="text-center">
                         <p class="text-2xl font-extrabold text-[#475569]">{{ data.puntaje }}%</p>
@@ -65,7 +84,7 @@ const dashOffset = props.data.porcentajeIncorrecto > props.data.porcentajeBlanco
                         <span class="ml-auto text-base font-semibold text-[#111827]">{{ data.incorrectas }}</span>
                     </div>
                     <div class="flex items-center gap-4">
-                        <div class="w-6 h-6 rounded-full bg-magenta_10 aspect-square"></div>
+                        <div class="w-6 h-6 rounded-full bg-[#CBD5E1] aspect-square"></div>
                         <p class="text-base text-[#475569]">En blanco:</p>
                         <span class="ml-auto text-base font-semibold text-[#111827]">{{ data.enblanco }}</span>
                     </div>
